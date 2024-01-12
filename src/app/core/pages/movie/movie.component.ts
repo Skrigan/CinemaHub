6bbceed0a696041c589092f6fb226056119ef267
Skrigan <sk_dm-821@mail.ru>
2024-01-12 16:34:14 +0300

    fix: movie page issues

--- src/app/core/pages/movie/movie.component.ts
+++ src/app/core/pages/movie/movie.component.ts
@@ -21,6 +21,7 @@ export class MovieComponent {
   // voidBoostLink?: SafeResourceUrl;
 
   trailerOrMovie?: SafeResourceUrl;
+  seasonsNumber?: string;
 
   isImgLoaded = false;
   constructor(private movieService: MovieService,
@@ -45,6 +46,8 @@ export class MovieComponent {
         this.movie = movie;
         console.log(this.movie);
 
+        this.seasonsNumber = this.getNumberOfSeasons(this.movie?.seasonsInfo?.length);
+
         const persons = this.movie.persons;
         this.actors = persons.filter((person: any) => {
           return person.enProfession === 'actor';
@@ -66,15 +69,28 @@ export class MovieComponent {
     }
   }
 
+  getNumberOfSeasons(seasonsLength: number) {
+    const lastSeasonsNum = seasonsLength % 10;
+    if (seasonsLength >= 10 && seasonsLength <= 20) {
+      return `${seasonsLength} сезонов`
+    } else if(lastSeasonsNum === 1) {
+      return `${seasonsLength} сезон`;
+    } else if(lastSeasonsNum >= 2 && lastSeasonsNum <= 4) {
+      return `${seasonsLength} сезона`;
+    } else if(lastSeasonsNum >= 5 && lastSeasonsNum <= 9 || lastSeasonsNum === 0) {
+      return `${seasonsLength} сезонов`;
+    } else {
+      return undefined;
+    }
+  }
+
   removeTrailerOrMovie() {
     this.trailerOrMovie = undefined;
   }
 
   getVoidBoostLink() {
-    //для нужных typeNums: ?s=1&e=1
-    //добавить в конце
     this.trailerOrMovie = this.sanitizer
-      .bypassSecurityTrustResourceUrl(`https://voidboost.tv/embed/${this.movie.id}`);
+      .bypassSecurityTrustResourceUrl(`https://voidboost.tv/embed/${this.movie.id}${this.movie.isSeries ? '?s=1&e=1' : ''}`);
   }
 
   onMovieChoice(event: MouseEvent) {
